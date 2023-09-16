d03ed4723b008d76b8b8d964c899b001fc570e2a
suraksha059 <surakshyat09@gmail.com>
2023-09-16 20:58:12 +0545

    first commit

---
+++ admin/common/footer.php
@@ -0,0 +1,9 @@
+ <!-- Footer Section Starts -->
+ <section class="footer">
+        <div class="wrapper text-center">
+            <p>Copyright <?php echo date('Y'); ?>.All Rights Reserves. Designed and Developed by <a class="footer-link" href="#">Suraksha Thapa</a></p>
+        </div>
+    </section>
+    <!-- Footer Section Ends -->
+</body>
+</html>
---
+++ layout/footer.php
@@ -0,0 +1,42 @@
+<!--Social Begins Here-->
+<section class="social text-center">
+        <div class="container">
+            <ul>
+                <li>
+                    <a href="#" target=" _blank">
+                        <img class="fb-img" src="https://img.icons8.com/color/48/000000/facebook-circled--v1.png"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" target=" _blank">
+                        <img class="yt-img" src="https://img.icons8.com/doodle/48/000000/youtube-play--v1.png"/>
+                    </a>
+                </li>
+                <li> 
+                    <a href="#">
+                        <img class="twit-img" src="https://img.icons8.com/fluency/48/000000/twitter.png"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" target=" _blank">
+                        <img class="insta-img" src="https://img.icons8.com/doodle/48/000000/instagram--v1.png"/>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!--Social Ends Here-->
+
+    <!--Footer Begins Here-->
+    <section class="footer text-center">
+        <div class="container">
+            <p>Copyright 2022. All Rights Reserved.
+                Designed by <a href="#" target=" _blank">Suraksha Thapa.</a>
+            </p>
+        </div>
+    </section>
+    <!--Footer Ends Here-->
+
+</body>
+
+</html>
